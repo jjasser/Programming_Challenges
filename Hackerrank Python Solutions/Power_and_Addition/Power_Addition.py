e713fb1acf41214f9dbc2db4e86384e51c9a0a99
yakav <jacobjasser@gmail.com>
2015-07-06 18:15:19 -0700

    Create Power_Addition.py

---
+++ Hackerrank Python Solutions/Power_and_Addition/Power_Addition.py
@@ -0,0 +1,9 @@
+#Takes four inputs, will calculate inputs of integers to powers then add them
+
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+e = pow(a,b) + pow(c,d)
+print e
